318d0f4accb61f9ce8a4b985120386a3beca055e
Arabei-Vadim <vadimarabei@mail.ru>
2022-01-16 13:15:36 +0300

    HW2

--- src/Message.tsx
+++ src/Message.tsx
@@ -7,12 +7,16 @@ type MessagePropsType = {
     time1: string
 }
 
-export const Message=(props: MessagePropsType)=> {
+export const Message = (props: MessagePropsType) => {
     return (
         <div>
-           <div><img src={props.avatar1} alt={'avo'} /> </div>
-            <span>{props.name1}</span>
-            <p>{props.message1}</p> <b>{props.time1}</b>
+            <div><img src={props.avatar1} alt={'avo'}/>
+                <span>{props.name1}</span>
+            </div>
+            <div><p>{props.message1}</p>
+                <b>{props.time1}</b>
+            </div>
+
         </div>
     )
 }
